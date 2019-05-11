b05266aeffbd909d316b2999b920ee751dda1586
tah-5 <lo0ol_95@hotmail.com>
2019-05-11 09:22:31 -0700

    Express Setup with Routes

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-var a = "Hello World";
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-function showOutput() {
-  document.getElementById("intro").innerHTML = a;
-}
+//Importing Cloud MongoDB Key
+const db = require("./config/keys").mongoURI;
+
+//Connecting to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+//Importing Route Files
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
+app.get("/", (req, res) => {
+  res.send("Index");
+});
+
+//Registering Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`);
+});
